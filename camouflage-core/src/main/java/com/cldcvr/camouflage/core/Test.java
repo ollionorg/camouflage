5fdd6ba0ef4af91c454bf595d7228b00d32b7c3b
Jaydeep Vaghasiya <jaydeep.vaghasiya-cloudcover@bukalapak.com>
2020-11-20 15:58:32 +0530

    hash config added

--- camouflage-core/src/main/java/com/cldcvr/camouflage/core/Test.java
+++ camouflage-core/src/main/java/com/cldcvr/camouflage/core/Test.java
@@ -2,12 +2,15 @@ package com.cldcvr.camouflage.core;
 
 import com.cldcvr.camouflage.core.info.types.AbstractInfoType;
 import com.cldcvr.camouflage.core.info.types.impl.PhoneNumber;
+import com.cldcvr.camouflage.core.mask.types.impl.HashConfig;
 import com.cldcvr.camouflage.core.mask.types.impl.ReplaceConfig;
 
 public class Test {
 
-
     public static void main(String[] args) {
+
+        AbstractInfoType phHc = new PhoneNumber(new HashConfig("123mysalt123"));
+        System.out.println(phHc.algorithm("hello world"));
 
         AbstractInfoType ph = new PhoneNumber(new ReplaceConfig("PHONE_NUMBER"));
         System.out.println(ph.algorithm("823423423"));
--- camouflage-core/src/main/java/com/cldcvr/camouflage/core/mask/types/impl/HashConfig.java
+++ camouflage-core/src/main/java/com/cldcvr/camouflage/core/mask/types/impl/HashConfig.java
@@ -3,23 +3,74 @@ package com.cldcvr.camouflage.core.mask.types.impl;
 import com.cldcvr.camouflage.core.mask.types.AbstractMaskType;
 
 import java.io.Serializable;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public final class HashConfig extends AbstractMaskType implements Serializable {
 
-    private final String replace;
     private final String salt;
-
-    public HashConfig(String replace,String salt)
+    private final byte[] saltBytes;
+    public HashConfig(String salt)
     {
-        this.replace=replace;
         this.salt=salt;
+        this.saltBytes = formatSalt();
     }
 
     public String name() {
         return "HASH_CONFIG";
     }
 
-    public String applyMaskStrategy(String input, String regex) {
-        return input.replace(regex,input.hashCode()+salt);
+    public byte[] formatSalt() {
+        byte[] saltBytes = this.salt.getBytes();
+        if (saltBytes.length > 16)
+            return Arrays.copyOfRange(saltBytes, 0, 16);
+        else return saltBytes;
+    }
+
+    public byte[] getSHA(String input) throws NoSuchAlgorithmException
+    {
+        // Static getInstance method is called with hashing SHA
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(this.saltBytes);
+        // digest() method called
+        // to calculate message digest of an input
+        // and return array of byte
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public  String toHexString(byte[] hash)
+    {
+        // Convert byte array into signum representation
+        BigInteger number = new BigInteger(1, hash);
+
+        // Convert message digest into hex value
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+
+        // Pad with leading zeros
+        while (hexString.length() < 32)
+        {
+            hexString.insert(0, '0');
+        }
+
+        return hexString.toString();
+    }
+
+    public String applyMaskStrategy(String input, String regex)
+    {
+        try {
+            return toHexString(getSHA(input));
+        }
+        catch (NoSuchAlgorithmException ex){
+            L
+        }
+        catch ( Exception ex )
+        {
+            System.out.println(ex);
+        }
+        return  "";
+
     }
 }
