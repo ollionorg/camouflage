0d4bcf8ecee3fb6377fcc802dbfd2b1e4dba96f6
Taher Koitawala <taherkoitawala@TaherKoitawala-2.local>
2020-11-30 17:16:27 +0530

    Empty check removed

--- camouflage-core/src/main/java/com/cldcvr/camouflage/core/mask/types/impl/ReplaceConfig.java
+++ camouflage-core/src/main/java/com/cldcvr/camouflage/core/mask/types/impl/ReplaceConfig.java
@@ -16,9 +16,9 @@ public class ReplaceConfig extends AbstractMaskType implements Serializable {
     }
 
     public String applyMaskStrategy(String input, String regex) {
-        if(input==null||input.equals(""))
+        if (input == null)
             return null;
-        return input.replaceAll(regex,replace);
+        return input.replaceAll(regex, replace);
 
     }
 
