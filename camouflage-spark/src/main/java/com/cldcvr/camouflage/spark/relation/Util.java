55678394e1841c6936f34f4c06730bb486ea5afb
Jaydeep Vaghasiya <jaydeep.vaghasiya-cloudcover@bukalapak.com>
2020-12-07 11:05:12 +0530

    cleanup for repo setup

--- camouflage-spark/src/main/java/com/cldcvr/camouflage/spark/relation/Util.java
+++
@@ -1,9 +0,0 @@
-package com.cldcvr.camouflage.spark.relation;
-
-public class Util {
-    public static void checkOrThrow(boolean condition, String message) throws IllegalArgumentException {
-        if (condition) {
-            throw new IllegalArgumentException(message);
-        }
-    }
-}
